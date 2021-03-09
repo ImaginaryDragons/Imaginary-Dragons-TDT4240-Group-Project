412d92090e72b71bfe4d835a9386782a2d748ff1
Elmeister <eldar.sandanger@gmail.com>
2021-03-09 22:27:18 +0100

    initialized project

---
+++ android/src/com/dragons/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.dragons.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.dragons.game.DragonsGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new DragonsGame(), config);
+	}
+}
---
+++ desktop/src/com/dragons/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.dragons.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.dragons.game.DragonsGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new DragonsGame(), config);
+	}
+}
