ace1db649dd81b13a1c5876eb46f579e21a6650d
jorgenmyrvold <jorgeum@stud.ntnu.no>
2021-03-18 11:55:32 +0100

    some implementation of playerControllerView

--- core/src/com/dragons/game/playerController/ControllerView.java
+++ core/src/com/dragons/game/playerController/ControllerView.java
@@ -1,7 +1,9 @@
 package com.dragons.game.playerController;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
 
 public class ControllerView {
 
@@ -10,12 +12,34 @@ public class ControllerView {
     private final Texture leftTexture = new Texture("leftArrow.PNG");
     private final Texture rightTexture = new Texture("rightArrow.PNG");
 
-    private Rectangle upButtonRectangle;
-    private Rectangle downButtonRectangle;
-    private Rectangle leftButtonRectangle;
-    private Rectangle rightButtonRectangle;
+    private final Rectangle upButtonRectangle;
+    private final Rectangle downButtonRectangle;
+    private final Rectangle leftButtonRectangle;
+    private final Rectangle rightButtonRectangle;
+
+    private final Rectangle allButtonRectangle;
 
     public ControllerView(int xPos, int yPos) {
+        int textureSize = upTexture.getHeight();  // Assumes all textures have equal size. In this case 80
+
+        allButtonRectangle = new Rectangle(xPos, yPos, 3*textureSize, 3*textureSize);
+
+        upButtonRectangle = new Rectangle(allButtonRectangle.x + textureSize, allButtonRectangle.y + 2*textureSize, upTexture.getWidth(), upTexture.getHeight());
+        downButtonRectangle = new Rectangle(allButtonRectangle.x + textureSize, allButtonRectangle.y, upTexture.getWidth(), upTexture.getHeight());
+        leftButtonRectangle = new Rectangle(allButtonRectangle.x, allButtonRectangle.y + textureSize, upTexture.getWidth(), upTexture.getHeight());
+        rightButtonRectangle = new Rectangle(allButtonRectangle.x + 2*textureSize, allButtonRectangle.y + textureSize, upTexture.getWidth(), upTexture.getHeight());
+    }
+
+    public void registerInput() {
+        Vector2 touch = new Vector2(Gdx.input.getX(), Gdx.input.getY());
+        boolean up = upButtonRectangle.contains(touch.x, touch.y);
+        boolean down = downButtonRectangle.contains(touch.x, touch.y);
+        boolean left = leftButtonRectangle.contains(touch.x, touch.y);
+        boolean right = rightButtonRectangle.contains(touch.x, touch.y);
+    }
+
+    public void render() {
         
     }
+
 }
--- core/src/com/dragons/game/utilities/Constants.java
+++ core/src/com/dragons/game/utilities/Constants.java
@@ -1,9 +1,12 @@
 package com.dragons.game.utilities;
 
+import com.badlogic.gdx.Gdx;
+
 public class Constants {
 
     public static final int PPM = 223;
-
+    public static final int WIDTH = Gdx.graphics.getWidth();
+    public static final int HEIGHT = Gdx.graphics.getHeight();
 
 
 }
