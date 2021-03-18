deacfca098058942f8208098b379b43987afc123
MadsBakken <76064866+MadsBakken@users.noreply.github.com>
2021-03-18 09:30:05 +0100

    Created skeletons for powerups

---
+++ core/src/com/dragons/game/powerups/IncreaseSpeedController.java
@@ -0,0 +1,21 @@
+package com.dragons.game.powerups;
+
+public class IncreaseSpeedController implements ControllerInterface {
+    PowerupsInterface increaseSpeed;
+    IncreaseSpeedView view;
+
+    public IncreaseSpeedController(PowerupsInterface increaseSpeed) {
+        this.increaseSpeed = increaseSpeed;
+
+    }
+
+    @Override
+    public void increaseSpeed() {
+
+    }
+
+    @Override
+    public void setSpeed() {
+
+    }
+}
---
+++ core/src/com/dragons/game/powerups/Powerups.java
@@ -0,0 +1,94 @@
+package com.dragons.game.powerups;
+
+import java.util.ArrayList;
+
+import javax.sound.midi.MetaEventListener;
+import javax.sound.midi.MetaMessage;
+
+
+public abstract class Powerups implements PowerupsInterface, MetaEventListener {
+        ArrayList speedObservers = new ArrayList();
+        int speed = 10;  //startfart
+        int bombs = 1; //antall bomber i starten
+        int bombRange = 1; //Hvor langt ilden går
+
+        @Override
+        public void initialize() {
+
+        }
+
+        @Override
+        public void increaseSpeed() {
+            speed += 2;
+        }
+
+        @Override
+        public void setSpeed(int speed) {
+            this.speed = speed;
+        }
+
+        @Override
+        public int getSpeed() {
+            return speed;
+        }
+
+        public void increaseBombRange() {
+            bombRange += 2;
+        }
+        public void setBombRange(int bombRange) {
+            this.bombRange = bombRange;
+        }
+
+        public int getBombRange() {
+            return bombRange;
+    }
+
+        public void increaseBombs() {
+            bombRange += 1;
+    }
+        public void setBombs(int bombs) {
+            this.bombs = bombs;
+    }
+
+        public int getBombs() {
+            return bombs;
+    }
+
+
+    @Override
+        public void registerObserver(speedObserver s) {
+
+        }
+
+        @Override
+        public void removeObserver(speedObserver s) {
+
+        }
+
+        @Override
+        public void registerObserver(bombsObserver s) {
+
+        }
+
+        @Override
+        public void removeObserver(bombsObserver s) {
+
+        }
+
+    @Override
+        public void registerObserver(bombRangeObserver s) {
+
+        }
+
+        @Override
+        public void removeObserver(bombRangeObserver s) {
+
+        }
+
+        @Override
+        public void meta(MetaMessage metaMessage) {
+
+        }
+    }
+
+}
