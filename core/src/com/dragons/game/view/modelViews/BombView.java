ffcc6bf22e35d0b1c56bdb8b3c493314ead9cad3
MadsBakken <76064866+MadsBakken@users.noreply.github.com>
2021-04-02 10:13:11 +0200

    some changes in bombview

--- core/src/com/dragons/game/view/modelViews/BombView.java
+++ core/src/com/dragons/game/view/modelViews/BombView.java
@@ -5,26 +5,38 @@ import com.dragons.game.model.bomb.Bomb;
 import com.dragons.game.utilities.AssetLoader;
 import com.dragons.game.utilities.Constants;
 
+import net.dermetfan.gdx.assets.AnnotationAssetManager;
+
+
 public class BombView {
     private Bomb bomb;
-    private Texture bombImg;
+    private Texture fireball;
     private Texture explosion;
     private float loadingTime;
+    private AnnotationAssetManager manager;
 
-    public BombView (){
+
+    public BombView(Bomb bomb, AnnotationAssetManager manager){
+        this.bomb = bomb;
+        this.manager = manager;
+        loadingTime = Constants.BombExplodeTime;
+        this.fireball = manager.get("FIREBALL");
+        this.explosion = manager.get("EXPLOSION");
         //bomb = new Texture("");
         //explosion = new Texture("");
         // while bombExplode == false
         // texture = bomb
         // bombExplode == false
         // texture = explosion
-        loadingTime = Constants.BombExplodeTime;
     }
 
     public void updateBomb() {
+        /*
          bombImg = AssetLoader.manager.get(AssetLoader.FIREBALL, Texture.class);
          explosion = AssetLoader.manager.get(AssetLoader.EXPLOSION, Texture.class);
          bomb.update(loadingTime);
+
+         */
 
     }
 
